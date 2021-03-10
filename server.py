9069e2506689fd5a58c5375b50090a0e324068a4
João Victor Naves <srjnaves@gmail.com>
2021-03-09 23:06:17 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,42 @@
+import socket 
+import threading
+import subprocess
+
+HEADER = 20
+PORT = 5056
+SERVER = '192.168.20.6'
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    while True:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                break
+            
+            output = subprocess.getoutput(msg).encode()
+            conn.send(output)
+
+    conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+
+print("[STARTING] server is starting...")
+start()
