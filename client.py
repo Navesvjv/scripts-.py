26cb5d9836465f4a87860f4a1e5851b2999e4303
João Victor Naves <srjnaves@gmail.com>
2021-03-09 23:28:11 -0300

    att
    Configurado para receber duas vezes, uma mensagem com o tamanho e outra a própria mensagem.

--- client.py
+++ client.py
@@ -10,18 +10,25 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-
-
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
+    
     client.send(send_length)
     client.send(message)
-    print(client.recv(HEADER).decode(FORMAT))
 
-command = input('Commando: ')
-send(command)
+    if msg != DISCONNECT_MESSAGE:
+        resp_len = client.recv(HEADER).decode(FORMAT)
+        resp_len = int(resp_len)
+        
+        response = client.recv(resp_len).decode(FORMAT)
+        print(response)
 
-send(DISCONNECT_MESSAGE)
+while True:
+    command = input('Commando: ')
+    if command == 'exit':
+        send(DISCONNECT_MESSAGE)
+        break    
+    send(command)
